267e0b5830960c5580d3d65c050da769c13a92e2
Maza92 <maza05599@gmail.com>
2024-09-13 18:23:36 -0500

    first commit

---
+++ src/test/java/api/proyect/manager/ManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package api.proyect.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
