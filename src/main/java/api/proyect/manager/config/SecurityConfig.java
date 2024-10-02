6a5f6ec10b3aea7d5fee104ab3aba76038cef9cb
Maza92 <maza05599@gmail.com>
2024-10-01 23:59:39 -0500

    cors config

--- src/main/java/api/proyect/manager/config/SecurityConfig.java
+++ src/main/java/api/proyect/manager/config/SecurityConfig.java
@@ -54,11 +54,12 @@ public class SecurityConfig {
 
   @Bean
   CorsConfigurationSource corsConfiguration() {
-    System.out.println("Cors asdf");
     CorsConfiguration configuration = new CorsConfiguration();
     configuration.setAllowedOrigins(List.of("*"));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS" ));
     configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+    configuration.setAllowedHeaders(List.of("Authorization"));
+    configuration.setAllowCredentials(true);
 
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/", configuration);
