cc44cf3e40908261558512aa74b3d174b12790ed
Maza92 <maza05599@gmail.com>
2024-10-02 00:38:07 -0500

    feat(*): cors added

--- src/main/java/api/proyect/manager/config/SecurityConfig.java
+++ src/main/java/api/proyect/manager/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,6 +21,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 import java.util.List;
 
@@ -34,35 +36,29 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
     return httpSecurity
-        .cors()
-        .and()
-        .csrf()
-        .disable()
-        .authorizeHttpRequests()
-        .requestMatchers("/api/auth/**", "api/health")
-        .permitAll()
-        .anyRequest()
-        .authenticated()
-        .and()
-        .sessionManagement()
-        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
-        .authenticationProvider(authenticationProvider)
-        .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
-        .build();
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authorizeRequests ->
+                    authorizeRequests.requestMatchers("/api/auth/**", "/api/health")
+                            .permitAll()
+                            .anyRequest()
+                            .authenticated()
+            )
+            .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+            .authenticationProvider(authenticationProvider)
+            .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+            .build();
   }
 
   @Bean
   CorsConfigurationSource corsConfiguration() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("*"));
+    configuration.setAllowedOrigins(List.of("http://localhost:5173"));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS" ));
     configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
-    configuration.setAllowedHeaders(List.of("Authorization"));
-    configuration.setAllowCredentials(true);
+    configuration.setAllowCredentials(false);
 
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/", configuration);
+    source.registerCorsConfiguration("/**", configuration);
     return source;
   }
 }
--- src/main/java/api/proyect/manager/controllers/LoginController.java
+++ src/main/java/api/proyect/manager/controllers/LoginController.java
@@ -8,14 +8,12 @@ import api.proyect.manager.services.AuthService;
 import api.proyect.manager.services.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
+@CrossOrigin
 public class LoginController {
 
     private final JwtService jwtService;
